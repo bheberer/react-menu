37855f60872973b5b6752059415ca8f335ef5d8a
bheberer <billyheb@gmail.com>
2019-11-30 21:51:20 -0800

    wow its already looking like a thing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
-import { Menu, MenuButton, MenuPopover } from './Menu';
+import {
+  Menu,
+  MenuButton,
+  MenuPopover,
+  MenuItems,
+  MenuItem,
+  MenuLink
+} from './Menu';
 
 export default function App() {
   return (
     <Menu>
       <MenuButton>Click Me</MenuButton>
-      <MenuPopover>Oh Hello</MenuPopover>
+      <MenuPopover>
+        <h3>Filters</h3>
+        <MenuItems>
+          <MenuItem>Products</MenuItem>
+          <MenuItem>Domains</MenuItem>
+          <MenuItem>Expiration Date</MenuItem>
+        </MenuItems>
+        <h3>Links</h3>
+        <MenuItems>
+          <MenuLink href="#">Settings</MenuLink>
+        </MenuItems>
+      </MenuPopover>
     </Menu>
   );
 }
