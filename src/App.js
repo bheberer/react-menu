5a20f8ca50ff80863c77c62ee11dedced0ded644
bheberer <billyheb@gmail.com>
2019-12-01 10:20:47 -0800

    lots of progress on keyboard accessibility

--- src/App.js
+++ src/App.js
@@ -28,3 +28,40 @@ export default function App() {
     </Menu>
   );
 }
+
+/* 
+So far, these components are HIGHLY composable, which is no surpise considering they mostly just render
+their children with some props.
+
+So, composability is cool, and you can basically make whatever variations you want, like for example, if you wanted to make a
+'MenuGroup' Component, you could do something like this:
+
+function MenuGroup({ title, children }) {
+  return (
+    <div>
+      <strong>{title}</strong>
+      {children}
+    </div>
+  )
+}
+
+And something like that is just a *little* bit more constrained. Feels more design-system-y and less library-y.
+Still pretty free-form as far as it goes, but you at least have SOME pre-determined shape.
+
+Could easily extend this to use the airbnb 'base-variant' pattern too.
+
+For example, the MenuButton could have an optional prop called icon, which would be a render prop. A lot of menu buttons have a little
+arrow icon or some other bit of UI on the right hand side of the button, so that's the space that this prop would populate.
+
+So you have:
+
+                 MenuButton
+BaseMenuButton /
+               \
+                MenuButtonWithIcon
+
+hyper-flexible -> feels like it belongs in a library, something you can use to build a design system, but not a design system itself
+Flexible, but constrained -> exposing the base, seems like it makes sense in a super big company with a lot of different products and
+UIs to build, makes it easier on the team as well because you don't need to think of every usable variation.
+Many, constrained options -> makes sense with a small system being used in a couple of similar products.
+*/
--- src/Menu.js
+++ src/Menu.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import { Machine } from 'xstate';
 import { useMachine } from '@xstate/react';
 
@@ -7,10 +7,10 @@ const menuMachine = Machine({
   initial: 'closed',
   states: {
     closed: {
-      on: { CLICK: 'open' }
+      on: { TOGGLE: 'open' }
     },
     open: {
-      on: { CLICK: 'closed' }
+      on: { TOGGLE: 'closed' }
     }
   }
 });
@@ -19,18 +19,44 @@ const MenuContext = React.createContext();
 
 export function Menu({ children }) {
   const [current, send] = useMachine(menuMachine);
+  const refContainer = useRef([]);
 
   return (
-    <MenuContext.Provider value={{ current, send }}>
+    <MenuContext.Provider value={{ current, send, refContainer }}>
       {children}
     </MenuContext.Provider>
   );
 }
 
 export function MenuButton({ children }) {
-  const { send } = useContext(MenuContext);
+  const { current, send, refContainer } = useContext(MenuContext);
+  const menuButtonRef = useRef();
 
-  return <button onClick={() => send('CLICK')}>{children}</button>;
+  useEffect(() => {
+    refContainer.current.push(menuButtonRef);
+  }, [refContainer]);
+
+  const handleKeyDown = e => {
+    if (e.key === 'ArrowDown' && current.matches('closed')) {
+      send('TOGGLE');
+    }
+    if (e.key === 'ArrowDown' && current.matches('open')) {
+      refContainer.current[1].current.focus();
+    }
+    if (e.key === 'Escape' && current.matches('open')) {
+      send('TOGGLE');
+    }
+  };
+
+  return (
+    <button
+      onClick={() => send('TOGGLE')}
+      onKeyDown={handleKeyDown}
+      ref={menuButtonRef}
+    >
+      {children}
+    </button>
+  );
 }
 
 export function MenuPopover({ children }) {
@@ -43,10 +69,81 @@ export function MenuItems({ children }) {
   return <div>{children}</div>;
 }
 
-export function MenuItem({ children }) {
-  return <button>{children}</button>;
+export function MenuItem({ children, onSelect }) {
+  const { current, send, refContainer } = useContext(MenuContext);
+  const itemRef = useRef();
+  const indexRef = useRef();
+  console.log(refContainer);
+
+  useEffect(() => {
+    refContainer.current.push(itemRef);
+    indexRef.current = refContainer.current.length - 1;
+  }, [refContainer]);
+
+  useEffect(() => {
+    if (
+      current.history.matches('closed') &&
+      current.matches('open') &&
+      indexRef.current === 1
+    ) {
+      itemRef.current.focus();
+    }
+  });
+
+  const handleKeyDown = e => {
+    if (e.key === 'ArrowDown') {
+      if (indexRef.current !== refContainer.current.length - 1) {
+        refContainer.current[indexRef.current + 1].current.focus();
+      }
+    }
+    if (e.key === 'ArrowUp') {
+      refContainer.current[indexRef.current - 1].current.focus();
+    }
+    if (e.key === 'Escape') {
+      refContainer.current[0].current.focus();
+      send('TOGGLE');
+    }
+  };
+
+  const select = () => {
+    onSelect();
+    send('TOGGLE');
+  };
+
+  return (
+    <button onClick={select} ref={itemRef} onKeyDown={handleKeyDown}>
+      {children}
+    </button>
+  );
 }
 
 export function MenuLink({ children, href }) {
-  return <a href={href}>{children}</a>;
+  const { refContainer } = useContext(MenuContext);
+  const itemRef = useRef();
+  const indexRef = useRef();
+  useEffect(() => {
+    refContainer.current.push(itemRef);
+    indexRef.current = refContainer.current.length - 1;
+  }, [refContainer]);
+
+  const handleKeyDown = e => {
+    if (e.key === 'ArrowDown') {
+      if (indexRef.current !== refContainer.current.length - 1) {
+        refContainer.current[indexRef.current + 1].current.focus();
+      }
+    }
+    if (e.key === 'ArrowUp') {
+      refContainer.current[indexRef.current - 1].current.focus();
+    }
+    if (e.key === 'Escape') {
+      refContainer.current[0].current.focus();
+      send('TOGGLE');
+    }
+  };
+
+  return (
+    <a href={href} ref={itemRef} onKeyDown={handleKeyDown}>
+      {children}
+    </a>
+  );
 }
