6b387fdb0ed8374a2797c4b187e827dc39c91ff1
bheberer <billyheb@gmail.com>
2019-12-01 18:23:58 -0800

    some general refinement + starting on clickOutside

--- src/Menu.js
+++ src/Menu.js
@@ -1,51 +1,97 @@
 import React, { useContext, useEffect, useRef, useLayoutEffect } from 'react';
-import { Machine } from 'xstate';
+import { Machine, assign, send } from 'xstate';
 import { useMachine } from '@xstate/react';
+
+// TODO: clear out ref list every render
+// TODO: set up click outside properly with an effect in 'Menu'
+
+// state machine with xstate, pretty simple, just wanted to tinker with xstate really
 
 const menuMachine = Machine({
   id: 'menu',
   initial: 'closed',
+  context: {
+    hovered: false
+  },
   states: {
     closed: {
       on: { TOGGLE: 'open' }
     },
     open: {
-      on: { TOGGLE: 'closed' }
+      on: {
+        TOGGLE: 'closed',
+        MOUSEOUT: {
+          actions: [
+            assign((context, event) => {
+              return { hovered: false };
+            })
+          ]
+        },
+        MOUSEIN: {
+          actions: [
+            assign((context, event) => {
+              return { hovered: true };
+            })
+          ]
+        },
+        CLICK: {
+          actions: [
+            (context, event) => {
+              console.log('hallo');
+              if (context.hovered) {
+                send('TOGGLE');
+              }
+            }
+          ]
+        }
+      }
     }
   }
 });
+
+/////////////////////////////////////////////////
+/* 
+Menu Component - Utilizes Compound Component pattern 
+with context to maximize flexibility.
+*/
 
 const MenuContext = React.createContext();
 
 export function Menu({ children }) {
   const [current, send] = useMachine(menuMachine);
-  const refContainer = useRef([]);
+  const menuItemRefs = useRef();
+  menuItemRefs.current = [];
+  const menuButtonRef = useRef();
 
   return (
-    <MenuContext.Provider value={{ current, send, refContainer }}>
-      {children}
+    <MenuContext.Provider
+      value={{ current, send, menuButtonRef, menuItemRefs }}
+    >
+      <div
+        onMouseLeave={() => send('MOUSEOUT')}
+        onMouseEnter={() => send('MOUSEIN')}
+        onMouseDown={() => send('CLICK')}
+      >
+        {children}
+      </div>
     </MenuContext.Provider>
   );
 }
 
 export function MenuButton({ children }) {
-  const { current, send, refContainer } = useContext(MenuContext);
-  const menuButtonRef = useRef();
-
-  useEffect(() => {
-    refContainer.current[0] = menuButtonRef;
-  }, [refContainer]);
+  const { current, send, menuItemRefs, menuButtonRef } = useContext(
+    MenuContext
+  );
 
   const handleKeyDown = e => {
     if (e.key === 'ArrowDown' && current.matches('closed')) {
       send('TOGGLE');
     }
     if (e.key === 'ArrowDown' && current.matches('open')) {
-      console.log(refContainer);
-      refContainer.current[1].current.focus();
+      menuItemRefs.current[0].current.focus();
     }
     if (e.key === 'Escape' && current.matches('open')) {
-      refContainer.current = refContainer.current.slice(0, 1);
+      // menuItemRefs.current;
       send('TOGGLE');
     }
   };
@@ -72,44 +118,48 @@ export function MenuItems({ children }) {
 }
 
 function useMenuItem() {
-  const { current, send, refContainer } = useContext(MenuContext);
+  const { current, send, menuItemRefs, menuButtonRef } = useContext(
+    MenuContext
+  );
   const itemRef = useRef();
   const indexRef = useRef();
 
   useLayoutEffect(() => {
-    indexRef.current = refContainer.current.length;
-    refContainer.current[indexRef.current] = itemRef;
-  }, [refContainer]);
+    indexRef.current = menuItemRefs.current.length;
+    menuItemRefs.current[indexRef.current] = itemRef;
+  }, [menuItemRefs]);
 
   useEffect(() => {
     if (
       current.history.matches('closed') &&
       current.matches('open') &&
-      indexRef.current === 1
+      indexRef.current === 0
     ) {
       itemRef.current.focus();
     }
   });
 
   const handleKeyDown = e => {
-    if (e.key === 'ArrowDown') {
-      if (indexRef.current !== refContainer.current.length - 1) {
-        refContainer.current[indexRef.current + 1].current.focus();
-      }
+    if (
+      e.key === 'ArrowDown' &&
+      indexRef.current !== menuItemRefs.current.length - 1
+    ) {
+      menuItemRefs.current[indexRef.current + 1].current.focus();
     }
-    if (e.key === 'ArrowUp') {
-      refContainer.current[indexRef.current - 1].current.focus();
+    if (e.key === 'ArrowUp' && indexRef.current > 0) {
+      menuItemRefs.current[indexRef.current - 1].current.focus();
     }
     if (e.key === 'Escape') {
-      refContainer.current[0].current.focus();
-      refContainer.current = refContainer.current.slice(0, 1);
+      menuButtonRef.current.focus();
+      // menuItemRefs.current = menuItemRefs.current.slice(0, 1);
       send('TOGGLE');
     }
   };
 
   const handleSelect = () => {
     onSelect();
-    refContainer.current = refContainer.current.slice(0, 1);
+    menuButtonRef.current.focus();
+    // menuItemRefs.current = menuItemRefs.current.slice(0, 1);
     send('TOGGLE');
   };
 
